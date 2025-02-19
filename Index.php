7906ce89d6090e0d6702b58d3c9c9097c561134d
JackOS <60794490+JackOSullivan003@users.noreply.github.com>
2025-02-19 20:46:17 +0000

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,8 +2,14 @@
 <html>
     <head>
         <title>PHP Test</title>
+        <style>
+            body {
+                background-color: Grey;
+            }
+        </style>
     </head>
     <body>
+        <h1>Welcome to the Game Shop!</h1>
         <?php
          echo "Hello World";
         ?>
