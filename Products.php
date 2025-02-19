1d03c1368ae720841bd41038ebf7fc6c6bb9255d
JackOS <60794490+JackOSullivan003@users.noreply.github.com>
2025-02-19 20:34:57 +0000

    Create Products.php

---
+++ Products.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php
+         echo "PRODUCTS GO HERE YIPEE";
+        ?>
+    </body>
+</html>
